574b1bd634be57488862d9ff5ab354862bdaa214
GustavoNevez <chips_gustavo2@outlook.com>
2024-03-05 18:40:13 -0300

    add confirming password

--- src/app/pages/signup/signup.page.ts
+++ src/app/pages/signup/signup.page.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AbstractControl, FormBuilder, FormGroup, ValidationErrors, Validators } from '@angular/forms';
 import { LoadingController } from '@ionic/angular';
 
 
@@ -12,6 +12,7 @@ import { LoadingController } from '@ionic/angular';
 export class SignupPage implements OnInit {
 
   regForm: FormGroup;
+  
 
   constructor(public formBuilder:FormBuilder,public loadingCtrl: LoadingController)   {}
 
@@ -21,26 +22,34 @@ export class SignupPage implements OnInit {
       email:['',[
         Validators.required,
         Validators.email,
-        Validators.pattern("/^[^\s@]+@[^\s@]+\.[^\s@]+$/")
+        Validators.pattern("[a-z0-9._%+\-]+@[a-z0-9.\-]+\.[a-z]{2,}$")
       ]],
       password:['',
       Validators.required,
       Validators.pattern("(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}")],
-    })
-   }
-
- get errorControl(){
-  return this.regForm?.controls;}
-async signUp(){
-  const loading = await this.loadingCtrl.create();
-  await loading.present();
-  if(this.regForm?.valid){
-    //const user = await this.authService.registerUser(email,password)
+      confirmPassword: ['', Validators.required],
+    }, { validator: this.passwordConfirming })
   }
-  
+
+  get errorControl(){
+    return this.regForm?.controls;}
+    
+  async signUp(){
+    const loading = await this.loadingCtrl.create();
+    await loading.present();
+    if(this.regForm?.valid){
+      //const user = await this.authService.registerUser(email,password)
+    }
+  }
+  passwordConfirming(group: FormGroup) {
+    const password = group.get('password').value;
+    const confirmPassword = group.get('confirmPassword').value;
+
+    return password === confirmPassword ? null : { mismatch: true };
+  }
 }
 
-}
+
 
   
 
